303ca187e3501fef5a727b88000862b6a342f4b9
mpapila <mehmetpapila95@gmail.com>
2024-11-08 12:34:59 -0600

    Refactor Sidebar for Responsive Design and Add Left Sidebar Modal

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,10 @@
-import { Box, CardContent, Typography } from "@mui/material";
+import {
+  Box,
+  CardContent,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import Person2Icon from "@mui/icons-material/Person2";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
@@ -11,17 +17,20 @@ import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { fetchAllNotifications } from "../redux/NotificationSlice";
 import { fetchUsersInfo, mergedIncomingRequests } from "../redux/UserSlice";
+import { setLeftSidebarOpen } from "../redux/PostFeedSlice";
 
 function Sidebar() {
+  const theme = useTheme();
+  const leftSidebarOpen = useSelector(
+    (state: RootState) => state.PostFeed.leftSidebarOpen
+  );
+  const isPhoneScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
   const dispatch = useDispatch<AppDispatch>();
   const token = localStorage.getItem("token");
   const myUserId = localStorage.getItem("myUserId");
   const mergedIncomingRequestsList = useSelector(
     (state: RootState) => state.UserInfo.mergedIncomingRequestsList
-  );
-
-  const activeButton = useSelector(
-    (state: RootState) => state.Sidebar.setActiveButton
   );
 
   const allUsersInfo = useSelector(
@@ -30,8 +39,6 @@ function Sidebar() {
 
   const navigate = useNavigate();
 
-  console.log("activeButton", activeButton);
-  console.log("mergedIncomingRequestsList", mergedIncomingRequestsList);
   useEffect(() => {
     dispatch(mergedIncomingRequests());
     dispatch(fetchAllNotifications());
@@ -45,64 +52,171 @@ function Sidebar() {
 
   return (
     <>
-      <CardContent
-        sx={{
-          height: "%100",
-          width: "25%",
-          minWidth: "25%",
-          backgroundColor: "#F6F6F6",
-          padding: "50px 0 0 0",
-          margin: "0",
-          minHeight: "80vh",
-        }}
-      >
-        {allUsersInfo.map((user) => (
-          <Box key={user._id}>
-            {user._id === myUserId ? (
-              <Typography
-                padding="20px 0 30px 30px"
-                fontWeight="fontWeightBold"
-                sx={{
-                  "&:hover": {
-                    cursor: "pointer",
-                    color: "#0043B7",
-                    backgroundColor: "#ededed",
-                  },
-                  lineHeight: "0.7",
-                }}
-              >
-                {user.firstName} {user.lastName}
-              </Typography>
-            ) : null}
-          </Box>
-        ))}
-        <TypographyButton onClick={() => navigate("/")}>
-          <FeedIcon sx={{ marginRight: "5px" }} /> Home
-        </TypographyButton>
-        <TypographyButton onClick={() => navigate(`/profile/${myUserId}`)}>
-          <Person2Icon sx={{ marginRight: "5px" }} /> Profile
-        </TypographyButton>
-        {mergedIncomingRequestsList.length < 1 ? (
-          <TypographyButton onClick={() => navigate("/notification")}>
-            <NotificationsNoneIcon sx={{ marginRight: "5px" }} /> Notification
-          </TypographyButton>
-        ) : (
-          <TypographyButton onClick={() => navigate("/notification")}>
-            <NotificationsActiveIcon sx={{ marginRight: "5px" }} />
-            Notification
-          </TypographyButton>
-        )}
-
-        <TypographyButton
-          onClick={() => {
-            localStorage.removeItem("myUserId");
-            localStorage.removeItem("token");
-            navigate("/login");
+      {!isPhoneScreen && (
+        <CardContent
+          sx={{
+            height: "%100",
+            width: "25%",
+            minWidth: "25%",
+            backgroundColor: "#F6F6F6",
+            padding: "50px 0 0 0",
+            margin: "0",
+            minHeight: "80vh",
           }}
         >
-          <LogoutIcon sx={{ marginRight: "5px" }} /> Logout
-        </TypographyButton>
-      </CardContent>
+          {allUsersInfo.map((user) => (
+            <Box key={user._id}>
+              {user._id === myUserId ? (
+                <Typography
+                  padding="20px 0 30px 30px"
+                  fontWeight="fontWeightBold"
+                  onClick={() => {
+                    navigate(`/profile/${myUserId}`);
+                  }}
+                  sx={{
+                    "&:hover": {
+                      cursor: "pointer",
+                      color: "#0043B7",
+                      backgroundColor: "#ededed",
+                    },
+                    lineHeight: "0.7",
+                  }}
+                >
+                  {user.firstName} {user.lastName}
+                </Typography>
+              ) : null}
+            </Box>
+          ))}
+          <TypographyButton onClick={() => navigate("/")}>
+            <FeedIcon sx={{ marginRight: "5px" }} /> Home
+          </TypographyButton>
+          <TypographyButton onClick={() => navigate(`/profile/${myUserId}`)}>
+            <Person2Icon sx={{ marginRight: "5px" }} /> Profile
+          </TypographyButton>
+          {mergedIncomingRequestsList.length < 1 ? (
+            <TypographyButton onClick={() => navigate("/notification")}>
+              <NotificationsNoneIcon sx={{ marginRight: "5px" }} /> Notification
+            </TypographyButton>
+          ) : (
+            <TypographyButton onClick={() => navigate("/notification")}>
+              <NotificationsActiveIcon sx={{ marginRight: "5px" }} />
+              Notification
+            </TypographyButton>
+          )}
+
+          <TypographyButton
+            onClick={() => {
+              localStorage.removeItem("myUserId");
+              localStorage.removeItem("token");
+              navigate("/login");
+            }}
+          >
+            <LogoutIcon sx={{ marginRight: "5px" }} /> Logout
+          </TypographyButton>
+        </CardContent>
+      )}
+      {leftSidebarOpen && (
+        <Box
+          display="block"
+          position="fixed"
+          top={50}
+          left={0}
+          height="50vh"
+          width="50vw"
+          p={2}
+          sx={{
+            backgroundColor: "white",
+            boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.3)",
+            zIndex: 10,
+            overflowY: "auto",
+          }}
+        >
+          <CardContent
+            sx={{
+              width: "100%",
+              height: "80%",
+              // minWidth: "25%",
+              backgroundColor: "#F6F6F6",
+              padding: "50px 0 0 0",
+              margin: "0",
+              // minHeight: "80vh",
+            }}
+          >
+            {allUsersInfo.map((user) => (
+              <Box key={user._id}>
+                {user._id === myUserId ? (
+                  <Typography
+                    padding="20px 0 30px 30px"
+                    fontWeight="fontWeightBold"
+                    onClick={() => {
+                      navigate(`/profile/${myUserId}`);
+                      dispatch(setLeftSidebarOpen(false));
+                    }}
+                    sx={{
+                      "&:hover": {
+                        cursor: "pointer",
+                        color: "#0043B7",
+                        backgroundColor: "#ededed",
+                      },
+                      lineHeight: "0.7",
+                    }}
+                  >
+                    {user.firstName} {user.lastName}
+                  </Typography>
+                ) : null}
+              </Box>
+            ))}
+            <TypographyButton
+              onClick={() => {
+                navigate("/");
+                dispatch(setLeftSidebarOpen(false));
+              }}
+            >
+              <FeedIcon sx={{ marginRight: "5px" }} /> Home
+            </TypographyButton>
+            <TypographyButton
+              onClick={() => {
+                dispatch(setLeftSidebarOpen(false));
+                navigate(`/profile/${myUserId}`);
+              }}
+            >
+              <Person2Icon sx={{ marginRight: "5px" }} /> Profile
+            </TypographyButton>
+            {mergedIncomingRequestsList.length < 1 ? (
+              <TypographyButton
+                onClick={() => {
+                  dispatch(setLeftSidebarOpen(false));
+                  navigate("/notification");
+                }}
+              >
+                <NotificationsNoneIcon sx={{ marginRight: "5px" }} />{" "}
+                Notification
+              </TypographyButton>
+            ) : (
+              <TypographyButton
+                onClick={() => {
+                  dispatch(setLeftSidebarOpen(false));
+                  navigate("/notification");
+                }}
+              >
+                <NotificationsActiveIcon sx={{ marginRight: "5px" }} />
+                Notification
+              </TypographyButton>
+            )}
+
+            <TypographyButton
+              onClick={() => {
+                localStorage.removeItem("myUserId");
+                localStorage.removeItem("token");
+                dispatch(setLeftSidebarOpen(false));
+                navigate("/login");
+              }}
+            >
+              <LogoutIcon sx={{ marginRight: "5px" }} /> Logout
+            </TypographyButton>
+          </CardContent>
+        </Box>
+      )}
     </>
   );
 }
