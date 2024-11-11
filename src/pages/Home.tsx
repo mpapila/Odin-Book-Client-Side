314d2a6eac391c7acd6777da544fe1e7643465a7
mpapila <mehmetpapila95@gmail.com>
2024-11-11 08:39:34 -0600

    removed unused imports and conditional rendering

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,16 +2,8 @@ import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
 import PostFeed from "../components/PostFeed";
 import Footer from "../components/Footer";
-import { useSelector } from "react-redux";
-import { RootState } from "../redux/Store";
-import Profile from "./Profile";
-import Notification from "./Notification";
-import PostPage from "./PostPage";
 
 function Home() {
-  const activeButton = useSelector(
-    (state: RootState) => state.Sidebar.setActiveButton
-  );
   return (
     <>
       <div>
@@ -19,12 +11,6 @@ function Home() {
         <div style={{ display: "flex" }}>
           <Sidebar />
           <PostFeed />
-          {/* <div style={{ padding: "20px" }}>
-            {activeButton === "home" && <PostFeed />}
-            {activeButton === "profile" && <Profile />}
-            {activeButton === "postpage" && <PostPage />}
-            {activeButton === "notification" && <Notification />}
-          </div> */}
         </div>
         <Footer />
       </div>
