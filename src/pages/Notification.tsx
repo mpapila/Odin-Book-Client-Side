47706725bcc64062b4b707e461b34ad5a1bd0308
mpapila <mehmetpapila95@gmail.com>
2024-11-11 08:54:56 -0600

    Enhance Notification Component with Improved Responsiveness and User Details - Added useMediaQuery and useTheme for dynamic sidebar rendering based on screen size. - Integrated conditionally rendered left sidebar for better responsive design. - Enhanced layout with personalized birthday greeting and emoji for own birthday.

--- src/pages/Notification.tsx
+++ src/pages/Notification.tsx
@@ -1,4 +1,10 @@
-import { Box, Button, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../redux/Store";
 import FaceIcon from "@mui/icons-material/Face";
@@ -17,30 +23,42 @@ import {
   setNotificationRead,
 } from "../redux/NotificationSlice";
 import { AllNotifications } from "../type";
+import { setMyName } from "../redux/UserProfileSlice";
 
 function Notification() {
+  const theme = useTheme();
+  const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
+  const myUserId = localStorage.getItem("myUserId");
+  const allUsersInfo = useSelector(
+    (state: RootState) => state.UserInfo.allUsers
+  );
+  const myName = useSelector((state: RootState) => state.UserProfile.myName);
+  allUsersInfo.map((user) => {
+    if (myUserId == user._id) {
+      dispatch(setMyName(user.firstName));
+    }
+  });
+  // console.log("myname", myName);
   const apiUrl = import.meta.env.VITE_API_URL;
   const URL = `${apiUrl}`;
   const token = localStorage.getItem("token");
-  const dispatch = useDispatch<AppDispatch>();
-  const isNotificationNew = useSelector(
-    (state: RootState) => state.Notification.setNotificationRead
-  );
   const allNotifications: AllNotifications = useSelector(
     (state: RootState) => state.Notification.allNotifications
   );
+
   console.log("birthday today", allNotifications.birthdaysToday);
+  console.log("allNotifications", allNotifications);
   const mergedIncomingRequestsList = useSelector(
     (state: RootState) => state.UserInfo.mergedIncomingRequestsList
   );
-  console.log("allNotifications", allNotifications);
+  // console.log("allNotifications", allNotifications);
 
   useEffect(() => {
     dispatch(mergedIncomingRequests());
     dispatch(fetchAllNotifications());
   }, []);
-  console.log("mergedIncomingRequestsList", mergedIncomingRequestsList);
+  // console.log("mergedIncomingRequestsList", mergedIncomingRequestsList);
 
   const mutationAcceptFriendRequest = useMutation({
     mutationFn: async (requestId) => {
@@ -70,6 +88,12 @@ function Notification() {
       },
     });
   };
+  const leftSidebarOpen = useSelector(
+    (state: RootState) => state.PostFeed.leftSidebarOpen
+  );
+  const isComputerScreen = useMediaQuery(theme.breakpoints.up("md"));
+  const isTabletScreen = useMediaQuery(theme.breakpoints.up("sm"));
+  const leftSidebar = isComputerScreen || isTabletScreen || leftSidebarOpen;
 
   return (
     <>
@@ -77,7 +101,7 @@ function Notification() {
       <div>
         <Header />
         <div style={{ display: "flex" }}>
-          <Sidebar />
+          {leftSidebar && <Sidebar />}
           <Box p="20px" minWidth="40vw">
             <Box mb={3} display="flex" flexDirection="column" gap={3}>
               <Typography fontWeight="800" fontSize="large">
@@ -145,7 +169,7 @@ function Notification() {
                     backgroundColor: "#ededed",
                   },
                 }}
-                color="GrayText"
+                // color="GrayText"
                 display="flex"
                 alignItems="center"
                 gap={1}
@@ -154,11 +178,27 @@ function Notification() {
                   allNotifications.birthdaysToday.map((notification, index) => (
                     <Box display="flex" alignItems="center" gap={1} key={index}>
                       <FaceIcon sx={{ fontSize: "50px" }} />
-                      <Box display="flex" flexDirection="row">
-                        <Typography fontWeight="bold">
-                          {notification.firstName} {notification.lastName}
-                        </Typography>
-                        <Typography>'s Birthday Today</Typography>
+                      <Box>
+                        {notification.firstName !== myName ? (
+                          <Box display="flex" flexDirection="row">
+                            <Typography fontWeight="bold">
+                              {notification.firstName} {notification.lastName}
+                            </Typography>
+                            <Typography>'s Birthday Today</Typography>
+                          </Box>
+                        ) : (
+                          <Box
+                            display="flex"
+                            flexDirection="row"
+                            alignItems="center"
+                          >
+                            <Typography>
+                              Happy Birthday, {notification.firstName}{" "}
+                              {notification.lastName}
+                            </Typography>
+                            <Typography fontSize="30px">🎉🎉</Typography>
+                          </Box>
+                        )}
                       </Box>
                     </Box>
                   ))}
@@ -183,7 +223,22 @@ function Notification() {
                     alignItems="center"
                     gap={1}
                   >
-                    <FaceIcon sx={{ fontSize: "50px" }} />
+                    {allUsersInfo.map((user) => {
+                      if (user._id == notification.userId) {
+                        return user.profilePhoto.length === 0 ? (
+                          <FaceIcon key={user._id} sx={{ fontSize: "50px" }} />
+                        ) : (
+                          <img
+                            key={user._id}
+                            src={user.profilePhoto}
+                            alt="Profile"
+                            width="40px"
+                          />
+                        );
+                      }
+                      return null;
+                    })}
+
                     <Box display="flex" gap={0}>
                       <Typography>{notification.message}</Typography>
                     </Box>
